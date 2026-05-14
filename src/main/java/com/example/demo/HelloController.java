74197b975d8efd5cc74cc7ec87ebb879c5548ab1
Tommy Ludwig <8924140+shakuzen@users.noreply.github.com>
2026-05-14 15:30:14 +0900

    Add hello world endpoint

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,4 +1,13 @@
 package com.example.demo;
 
-public class HelloController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class HelloController {
+
+    @GetMapping
+    String sayHello() {
+        return "Hello World";
+    }
 }
