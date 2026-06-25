0709127a5f69dd8c2c23a48e8a2f92373cccd574
Tommy Ludwig <8924140+shakuzen@users.noreply.github.com>
2026-06-25 13:27:37 +0900

    Use CPUSet instead of quota
    Hopefully this gives more consistent results, although I'm not sure if it should matter.

--- run-benchmark-in-docker.sh
+++ run-benchmark-in-docker.sh
@@ -14,4 +14,4 @@ echo "Building Docker image..."
 docker build -t java-memory-comparison .
 
 echo "Running benchmark in Docker..."
-docker run --rm --cpus=2 --memory=2g -v "$(pwd):/work" java-memory-comparison ./benchmark.sh "$@"
+docker run --rm --cpuset-cpus="0,1" --memory=2g -v "$(pwd):/work" java-memory-comparison ./benchmark.sh "$@"
